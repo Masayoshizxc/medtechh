52e1f9fd04d5eff72a01fcc85d3b3a35155b1f95
Eldiiar Kudaiberdiev <eldiiar.kudaiberdiev@alatoo.edu.kg>
2022-05-31 14:23:05 +0600

    Intial commit

---
+++ MedTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedTech
+//
+//  Created by Eldiiar on 31/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
