70030359435df413fdf8c77a1354af3d934cc701
Adilet <masayoshizxc@gmail.com>
2022-07-11 14:31:12 +0600

    refactoring tableview

---
+++ MedTech/Screens/Main/TableView/AppointmentTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AppointmentTableViewCell.swift
+//  MedTech
+//
+//  Created by Adilet on 10/7/22.
+//
+
+import UIKit
+
+class AppointmentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
